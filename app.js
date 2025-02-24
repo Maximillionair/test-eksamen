b1912157c8df9ce0ba45602365ae22beb27308d6
Maximillionair <maxmillian.heim@outlook.com>
2025-02-24 12:50:02 +0100

    prøver alternativ løsning og hadde glemt å forandre på importerings metode

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const path = require("path");
 const app = express();
 // const {connectDB} = require('./handlers/dbHandler.js');
 
+const indexRoutes = require("./routes/indexRoutes");
+const userRoutes = require("./routes/userRoutes");
+const reindeerRoutes = require("./routes/reindeerRoutes");
+const flockRoutes = require("./routes/flockRoutes");
+const areaRoutes = require("./routes/areaRoutes");
+
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", 'ejs');
 
@@ -16,9 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // To handle URL-encoded data
-app.use(routes);
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 
+// using the individual routers
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
+app.use("/reindeer", reindeerRoutes);
+app.use("/flock", flockRoutes);
+app.use("/map", areaRoutes);
 
 // Start the server on port 3500
 const PORT = process.env.PORT || 3500;
