0eb8b12d80cdece3d639b2b71507c8a53efd67a5
Maximillionair <maxmillian.heim@outlook.com>
2025-02-26 14:45:05 +0100

    Update reindeerModel.js

--- models/reindeerModel.js
+++ models/reindeerModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const ReindeerSchema = new mongoose.Schema({
-  serialNumber: { type: String, required: true, unique: true },
-  name: String,
-  flock: { type: mongoose.Schema.Types.ObjectId, ref: "Flock",  },
-  age: Number,
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "Owner",  }
+  serialNumber: { type: String, required: true },
+  name: { type: String, required: true },
+  flock: { type: mongoose.Schema.Types.ObjectId, ref: "Flock" }, // Flock remains an ObjectId
+  age: { type: Number, required: true },
+  owner: { type: String, required: true }, // Store owner's name instead of ObjectId
 });
 
 module.exports = mongoose.model("Reindeer", ReindeerSchema);
